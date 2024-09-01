91243e5178f04df5dc6d9c5ea6e3a80a81bbcb9b
Guilhermeaug <guilhermeaugustodeoliveira66@gmail.com>
2024-09-01 19:01:59 -0300

    improve security for groups

--- src/api/group/controllers/group.js
+++ src/api/group/controllers/group.js
@@ -1,9 +1,40 @@
-'use strict';
+"use strict";
 
 /**
  * group controller
  */
+const { createCoreController } = require("@strapi/strapi").factories;
 
-const { createCoreController } = require('@strapi/strapi').factories;
+module.exports = createCoreController("api::group.group", ({ strapi }) => ({
+  async find(ctx) {
+    const user = ctx.state.user;
 
-module.exports = createCoreController('api::group.group');
+    ctx.query.filters = {
+      ...(ctx.query.filters || {}),
+      teacher: user.id,
+    };
+
+    return super.find(ctx);
+  },
+  async findOne(ctx) {
+    const user = ctx.state.user;
+    const entryId = ctx.params.id;
+    let entry = {};
+
+    if (entryId) {
+      entry = await strapi.entityService.findOne("api::group.group", entryId, {
+        populate: {
+          teacher: {
+            fields: ["id"],
+          },
+        },
+      });
+    }
+
+    if (!entry || entry.teacher.id !== user.id) {
+      return ctx.unauthorized("You are not authorized to access this entry");
+    }
+
+    return super.findOne(ctx);
+  },
+}));
